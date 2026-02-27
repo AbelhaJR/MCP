c3a18d77150c697b0b17f7846fd7fd549ad4fa4c
Henrique Abelha <44907358+AbelhaJR@users.noreply.github.com>
2026-02-27 09:49:46 +0000

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,7 +1,8 @@
 import azure.functions as func
-from mcp import asgi_app
+from mcp_server import asgi_app
 
 app = func.AsgiFunctionApp(
     app=asgi_app,
     http_auth_level=func.AuthLevel.ANONYMOUS
+
 )
